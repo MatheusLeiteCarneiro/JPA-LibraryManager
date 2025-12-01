5b381600c77185e7ba10695af0376b44679225c6
Matheus <matheus.leite.carneiro@gmail.com>
2025-12-01 08:55:28 -0300

    feat: Add the entity classes attributes, methods and annotations

--- src/main/java/com/librarymanager/model/Author.java
+++ src/main/java/com/librarymanager/model/Author.java
@@ -1,4 +1,49 @@
 package com.librarymanager.model;
 
+import jakarta.persistence.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
 public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "author", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private Set<Book> books = new HashSet<>();
+
+    public Author() {
+    }
+
+    public Author(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void addBook(Book book) {
+        this.books.add(book);
+        book.setAuthor(this);
+    }
+
+    public void removeBook(Book book) {
+        this.books.remove(book);
+        book.setAuthor(null);
+    }
+
 }
--- src/main/java/com/librarymanager/model/Book.java
+++ src/main/java/com/librarymanager/model/Book.java
@@ -1,4 +1,56 @@
 package com.librarymanager.model;
 
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
 public class Book {
+
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String name;
+
+    @Column(name = "Publication_Date")
+    private LocalDate publicationDate;
+
+    @ManyToOne (fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "author_id")
+    private Author author;
+
+    public Book() {
+    }
+
+    public Book(String name, LocalDate publicationDate, Author author) {
+        this.name = name;
+        this.publicationDate = publicationDate;
+        this.author = author;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getPublicationDate() {
+        return publicationDate;
+    }
+
+    public void setPublicationDate(LocalDate publicationDate) {
+        this.publicationDate = publicationDate;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
 }
