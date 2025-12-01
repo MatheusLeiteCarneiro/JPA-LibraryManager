187a6cf320fedf1f307a3d98b28fbb076e31d0b3
Matheus <matheus.leite.carneiro@gmail.com>
2025-12-01 12:43:14 -0300

    feat: add new book operation case

--- src/main/java/com/librarymanager/Main.java
+++ src/main/java/com/librarymanager/Main.java
@@ -11,6 +11,12 @@ import jakarta.persistence.Persistence;
 import org.h2.tools.Server;
 
 import java.sql.SQLException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
@@ -20,15 +26,16 @@ public class Main {
             server.start();
             EntityManagerFactory emf = Persistence.createEntityManagerFactory("library-jpa");
             EntityManager em = emf.createEntityManager();
-
-
+            Scanner sc = new Scanner(System.in);
+            mainMenu(sc, em);
+            sc.close();
         } catch (SQLException e) {
             throw new DBException(e.getMessage());
         }
 
     }
 
-    private void mainMenu(Scanner sc, EntityManager em) {
+    private static void mainMenu(Scanner sc, EntityManager em) {
         int selector = -1;
         while (selector != 0) {
             System.out.println("(MAIN MENU)");
@@ -53,13 +60,15 @@ public class Main {
         }
     }
 
-    private void authorMenu(Scanner sc, EntityManager em){
+    private static void authorMenu(Scanner sc, EntityManager em){
         AuthorDAO authorDAO = new AuthorDAO(em);
         Author author = null;
         System.out.println("(AUTHORS MENU)");
         System.out.println("DIGIT:");
         System.out.println("1- Create new Author | 2- Find Author Books | 3- List All Authors | 4- Update Author | 5- Delete Author");
-        switch (sc.nextInt()) {
+        int operation = sc.nextInt();
+        sc.nextLine();
+        switch (operation) {
             case 1:
                 System.out.print("Author Name:");
                 String name = sc.nextLine();
@@ -104,14 +113,33 @@ public class Main {
         }
     }
 
-    private void bookMenu(Scanner sc, EntityManager em){
+    private static void bookMenu(Scanner sc, EntityManager em){
+        DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy");
         BookDAO bookDAO = new BookDAO(em);
+        AuthorDAO authorDAO = new AuthorDAO(em);
         Book book = null;
+        Author author = null;
         System.out.println("(BOOKS MENU)");
         System.out.println("DIGIT:");
         System.out.println("1- Create a new Book | 2- Find a Book By ID | 3- List All Books | 4- Update a Book | 5- Delete a Book");
-        switch (sc.nextInt()) {
+        int operation = sc.nextInt();
+        sc.nextLine();
+        switch (operation) {
             case 1:
+                System.out.print("Book name: ");
+                String name = sc.nextLine();
+                System.out.print("Publication date (dd/MM/yyyy): ");
+                LocalDate publicationDate = LocalDate.parse(sc.next(),fmt);
+                System.out.print("Author ID:");
+                author = authorDAO.getAuthorByID(sc.nextLong());
+                if(idFound(author)){
+                    book = new Book(name, publicationDate, author);
+                    bookDAO.addBook(book);
+                    System.out.println("Book successfully added");
+                }
+                else {
+                    System.out.println("Operation canceled, author not found!");
+                }
                 break;
             case 2:
                 break;
@@ -126,7 +154,7 @@ public class Main {
         }
     }
 
-    private <T> boolean idFound(T type){
+    private static <T> boolean idFound(T type){
         if(type == null){
             System.err.println("ID not found!");
             return false;
